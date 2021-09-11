40d647bc02006dc4178473cd5295cba22742e5c5
vahidGhadiri <whydrf@gmail.com>
2021-09-11 21:40:16 +0430

    Palindrome - Solution #1

---
+++ Algos/Palindrome/index.js
@@ -0,0 +1,21 @@
+//The Question is:
+//Given a string, return true if the string is a palindrome or false if it is not
+//Palindromes are string that form the same word if it is reversed.
+// *DO* include spaces and punctuation in determining if the string is a palindrome.
+//Example:
+//palindrome abba = true
+//palindrome baba = false
+
+
+//Solution #1
+const palindrome = (str) => {
+    let result = ""
+    for (let char of str) {
+        result = char + result
+    }
+
+    return result === str
+}
+
+
+module.exports = palindrome
---
+++ Algos/Palindrome/test.js
@@ -0,0 +1,10 @@
+const palindrome = require("./index")
+
+describe("PALINDROME ALGORITHM", () => {
+
+    test("Palindrome function should be defined", () => {
+        expect(palindrome).toBeDefined()
+    })
+
+
+})
